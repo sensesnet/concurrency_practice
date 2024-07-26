a2e70993f7cd63229e2df915fa4f87828d9d75f4
sensesnet <sensesnet@gmail.com>
2024-07-26 10:57:34 +0200

    [Thread SEMAPHORE] practice

---
+++ src/main/java/org/example/semaphore/Runner.java
@@ -0,0 +1,13 @@
+package org.example.semaphore;
+
+public final class Runner {
+    public static void main(final String... args) {
+        final int poolSize = 3;
+        final ConnectionPool pool = new ConnectionPool(poolSize);
+
+        final ConnectionPoolTask poolTask = new ConnectionPoolTask(pool);
+        final int threadCount = 15;
+        final Thread[] threads = ThreadUtil.createThreads(poolTask, threadCount);
+        ThreadUtil.startThreads(threads);
+    }
+}
