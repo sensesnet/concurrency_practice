be1887db589394a7998c87f665704264fe427f06
sensesnet <sensesnet@gmail.com>
2024-07-30 00:44:49 +0200

    [Thread CountDownLatch] practice

---
+++ src/main/java/org/example/countdownlatch/ThreadUtil.java
@@ -0,0 +1,14 @@
+package org.example.countdownlatch;
+
+import static java.util.Arrays.stream;
+
+public final class ThreadUtil {
+
+    public static void startThreads(final Thread[] threads) {
+        stream(threads).forEach(Thread::start);
+    }
+
+    private ThreadUtil() {
+        throw new UnsupportedOperationException();
+    }
+}
