89ea515dcd99f9de6fe7cdf5d1e9aa991483630a
Degtyarev Kirill <sensesnet@gmail.com>
2024-07-31 23:53:21 +0200

    [Exchanger] practice

---
+++ src/main/java/org/example/exchanger/ExchangedObject.java
@@ -0,0 +1,14 @@
+package org.example.exchanger;
+
+public final class ExchangedObject {
+    private final long id;
+
+    public ExchangedObject(final long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getName() + "[id = " + this.id + "]";
+    }
+}
---
+++ src/main/java/org/example/exchanger/ExchangedObjectFactory.java
@@ -0,0 +1,16 @@
+package org.example.exchanger;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public final class ExchangedObjectFactory {
+    private long nextId;
+
+    public ExchangedObject create() {
+        try {
+            SECONDS.sleep(2);
+            return new ExchangedObject(this.nextId++);
+        } catch (final InterruptedException cause) {
+            throw new RuntimeException(cause);
+        }
+    }
+}
