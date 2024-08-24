18b1ba8def453e836045cb4ffcd3f442f1a6a2f2
Degtyarev Kirill <sensesnet@gmail.com>
2024-08-24 11:25:50 +0200

    [Phaser] practice

---
+++ src/main/java/org/example/phaser/Subtask.java
@@ -0,0 +1,23 @@
+package org.example.phaser;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public final class Subtask extends CompositeTask<AbstractLeafTask> {
+
+    public Subtask(final long id, final List<LeafTask> leafTasks, final LastLeafTask lastLeafTask) {
+        super(id, concat(leafTasks, lastLeafTask));
+    }
+
+    @Override
+    protected void perform(final AbstractLeafTask leafTask) {
+        leafTask.perform();
+    }
+
+    private static List<AbstractLeafTask> concat(final List<LeafTask> leafTasks, final LastLeafTask lastLeafTask) {
+        return Stream.concat(
+                leafTasks.stream(),
+                Stream.of(lastLeafTask)
+        ).toList();
+    }
+}
---
+++ src/main/java/org/example/phaser/Task.java
@@ -0,0 +1,16 @@
+package org.example.phaser;
+
+public abstract class Task {
+    private final long id;
+
+    public Task(final long id) {
+        this.id = id;
+    }
+
+    public abstract void perform();
+
+    @Override
+    public final String toString() {
+        return getClass().getName() + "[id = " + id + "]";
+    }
+}
